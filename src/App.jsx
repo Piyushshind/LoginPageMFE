8e959f289940fe927a1f9c2e656c60d2e28b4acf
Kuldeep Dhaka <kuldeep.dhaka@techinfinito.com>
2023-02-24 02:14:25 +0530

    mfes attached

--- src/App.jsx
+++ src/App.jsx
@@ -11,41 +11,43 @@ import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import Timepass from "./Timepass";
 import Banner4 from "./components/views/Banner4/Banner4";
 import MyButton from './components/views/MyButton/MyButton'
-import { BrowserRouter, Route,Routes } from "react-router-dom";
-import HomePage from "./components/views/HomePage/HomePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "HomePageMFE/Hero";
 import Centerlizer from "./components/views/Centerlizer/Centerlizer";
+import SingleProductPage from 'ProductPageMFE/SingleProductCard'
+
+const App = () => {
 
 
-const App = () =>{
-  
- 
   return (
-   <BrowserRouter>
-    < >
-    <div >
-    <Header />
-      <Navbar />
-      <Centerlizer>
-      <Routes>
-     <Route path='/' element={<HomePage /> }/>
-      <Route path='/login' element={<LoginPage /> }/>
-      <Route path='/signIn' element={<RegisterPage />}/>
-     </Routes>
-    
+    <BrowserRouter>
+      < >
+        <div >
+          <Header />
+          <Navbar />
+          <Centerlizer>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/signIn' element={<RegisterPage />} />
+              <Route path='/product' element={<SingleProductPage />} />
+              <Route path='/checkout' element={<SingleProductPage />} />
+            </Routes>
 
-      </Centerlizer>
-    
-      
-    
-      {/* <ProductCard /> */}
-      
-      
-       {/* <Hero/>  */}
-      {/* <Banner4>{<Text>Bhavish</Text>}
+
+          </Centerlizer>
+
+
+
+          {/* <ProductCard /> */}
+
+
+          {/* <Hero/>  */}
+          {/* <Banner4>{<Text>Bhavish</Text>}
       {<MyButton>Shop Now</MyButton>}</Banner4> */}
-      </div>
-    </>
+        </div>
+      </>
     </BrowserRouter>
   );
-} 
+}
 ReactDOM.render(<App />, document.getElementById("app"));
