51427053aac914db3b306254d642845945879001
Piyushshind <piyushshinde.199@gmail.com>
2023-03-02 11:47:29 +0530

    changes in routing

--- src/components/views/RoutingComponant/RoutingComponant.jsx
+++ src/components/views/RoutingComponant/RoutingComponant.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import classes from './RoutingComponant.module.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Centerlizer from '../Centerlizer/Centerlizer';
 import HomePage from '../HomePage/HomePage';
 import LoginPage from '../LoginPage/LoginPage';
@@ -22,27 +22,37 @@ import useMfeStore from '../../../utils/zustand';
 
 
 export const RoutingComponant = (props) => {
-    const { routes = [{
-        path: "/", element: <HomePage />,
-        path: "/login", element: <LoginPage />,
-        path: "/signIn", element: <RegisterPage />,
-        path: "/product", element: <SingleProductPage />,
-        path: "/checkout", element: <CheckoutPage />,
-    }] } = props;
+    // const { routes = [{
+    //     path: "/", element: <HomePage />,
+    //     path: "/login", element: <LoginPage />,
+    //     path: "/signIn", element: <RegisterPage />,
+    //     path: "/product", element: <SingleProductPage />,
+    //     path: "/checkout", element: <CheckoutPage />,
+    //     path: "*", element: <ErrorPage />,
+
+    // }] } = props;
     return (
         <>
             <Centerlizer>
-                <Routes>
-                    {routes.map(route =>
-                        <Route path={route.path} element={route.element} />
-                    )}
+                <Routes >
                     <Route path="*" element={<ErrorPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="signIn" element={<RegisterPage />} />
+                    <Route path="product" element={<SingleProductPage />} />
                 </Routes>
             </Centerlizer>
 
         </>
     )
 }
+
+
+
+
+// {routes.map((route,i )=>
+//     <Route key={i} path={route.path} element={route.element} />
+// )}
 
 /**
  * Property type definations
